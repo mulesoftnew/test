f227100995a8631209e57e6e931c97054f0f370c
mulesoftnew <naturetrails528@gmail.com>
2026-04-30 13:10:31 -0400

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,1 +1,14 @@
-print("hi")
+import datetime
+
+# Get the current system time
+current_time = datetime.datetime.now()
+Client="1a2b3c4d-5e6f-7g8h-9i0j-k1l2m3n4o5p6"
+Tenant="09876543-abcd-efgh-ijkl-1234567890ab"
+secret="_A1b2C3d~E4f5G6h7I8j9K0l.M1n2O35p"
+secret="_A1b2C3d~E4f5G6h7I8j9K0l.M1n2O34p"
+secret="_A1b2C3d~E4f5G6h7I8j9K0l.M1n2O3p3"
+
+secret="_A1b2C3d~E4f5G6h7I8j9K0l.M1n2O3p2"
+
+# Print the current time
+print("Current system time:", current_time)
